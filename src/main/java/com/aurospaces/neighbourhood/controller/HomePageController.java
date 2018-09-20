aef21cd4be121ba907b0b31be18530ac8d2d134a
kotaiahandrjau <kotaiahandraju@gmail.com>
2018-09-20 17:57:03 +0530

    full profile count

--- src/main/java/com/aurospaces/neighbourhood/controller/HomePageController.java
+++ src/main/java/com/aurospaces/neighbourhood/controller/HomePageController.java
@@ -5420,6 +5420,8 @@ public String premiumMembers(@ModelAttribute("createProfile") UsersBean searchCr
 			if(age_from<=user_age && user_age<=age_to){
 				match_score += 8;
 			}
+		}else {
+			match_score += 8;
 		}
 		
 		String r_height_from = profileObj.get("rHeight");
@@ -5431,6 +5433,8 @@ public String premiumMembers(@ModelAttribute("createProfile") UsersBean searchCr
 			if(height_from<=user_height && user_height<=height_to){
 				match_score += 8;
 			}
+		}else {
+			match_score += 8;
 		}
 		
 		String r_val = profileObj.get("rMaritalStatus");
@@ -5478,10 +5482,10 @@ public String premiumMembers(@ModelAttribute("createProfile") UsersBean searchCr
 		user_val = userBean.getCurrentState();
 		addedScore = this.getMatchScoreOf(r_val, user_val);
 		match_score += addedScore;
-		r_val = profileObj.get("rCity");
+		/*r_val = profileObj.get("rCity");
 		user_val = userBean.getCurrentCity();
 		addedScore = this.getMatchScoreOf(r_val, user_val);
-		match_score += addedScore;
+		match_score += addedScore;*/
 		r_val = profileObj.get("rDiet");
 		user_val = userBean.getDiet();
 		addedScore = this.getMatchScoreOf(r_val, user_val);
