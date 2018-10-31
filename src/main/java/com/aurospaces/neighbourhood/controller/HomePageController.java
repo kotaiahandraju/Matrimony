c4a29e02cd91792cfc4e8bdb263f1945cb531a6c
kotaiahandraju <a.kotaiah12@gmail.com>
2018-10-31 16:29:47 +0530

    otp after login issue -- fixed

--- src/main/java/com/aurospaces/neighbourhood/controller/HomePageController.java
+++ src/main/java/com/aurospaces/neighbourhood/controller/HomePageController.java
@@ -3696,8 +3696,12 @@ String sJson="";
 						profileObj.put("photosList", "");
 					}
 					//get profile match score
-					int match_score = this.getProfileMatchScore(userBean, profileObj);
-					profileObj.put("match_score", match_score+"");
+					try{
+						int match_score = this.getProfileMatchScore(userBean, profileObj);
+						profileObj.put("match_score", match_score+"");
+					}catch(Exception exp){
+						exp.printStackTrace();profileObj.put("match_score", "exception");
+					}
 				}
 				
 				objectMapper = new ObjectMapper();
@@ -3903,36 +3907,44 @@ String sJson="";
 				objeModel.addAttribute("createProfile", savedUserData);
 				session.setAttribute("profileToBeCreated", savedUserData);
 				//String mobileNum = objUserssBean.getMobile();
+				/*************
+				check OTP limit for the day
 				
-				String otp = objUsersDao.genOtp();
-			   //insert into otp table
-				request.setAttribute("mobileStr", mobileNum.substring(mobileNum.length()-3));	
-			   boolean success = objUsersDao.saveOtp(sessionBean.getId()+"",mobileNum,otp);
-			   if(success){
-				   try{
-					   String response = SendSMS.sendSMS("Dear "  +sessionBean.getFirstName()+ " "+sessionBean.getLastName()+",\nThanks for registering with Aarna Matrimony. OTP for your registration is: "+otp, mobileNum);
-					   
-					   if("OK".equalsIgnoreCase(response)){
+				*************/
+			   int count = objUsersDao.getOTPCount(mobileNum);
+			   if(count<MatrimonyConstants.OTP_LIMIT){
+					String otp = objUsersDao.genOtp();
+				   //insert into otp table
+					request.setAttribute("mobileStr", mobileNum.substring(mobileNum.length()-3));	
+				   boolean success = objUsersDao.saveOtp(sessionBean.getId()+"",mobileNum,otp);
+				   if(success){
+					   try{
+						   String response = SendSMS.sendSMS("Dear "  +sessionBean.getFirstName()+ " "+sessionBean.getLastName()+",\nThanks for registering with Aarna Matrimony. OTP for your registration is: "+otp, mobileNum);
 						   
-						   request.setAttribute("message", "success");
-					   }else{
-						   request.setAttribute("message", "failed"); 
+						   if("OK".equalsIgnoreCase(response)){
+							   
+							   request.setAttribute("message", "success");
+						   }else{
+							   request.setAttribute("message", "failed"); 
+						   }
+						   //throw new Exception();
+					   }catch(Exception e){
+						   e.printStackTrace();
+						   request.setAttribute("message", "failed");
+						   //objUsersDao.delete(sessionBean.getId());
 					   }
-					   //throw new Exception();
-				   }catch(Exception e){
-					   e.printStackTrace();
+					   
+					   return "otpPage";
+					   /*objeModel.addAttribute("createProfile", objUserssBean);ff
+					   return "redirect:saveUserProfile";*/
+				   }else{
 					   request.setAttribute("message", "failed");
-					   //objUsersDao.delete(sessionBean.getId());
+					   return "otpPage";
 				   }
-				   
-				   return "otpPage";
-				   /*objeModel.addAttribute("createProfile", objUserssBean);ff
-				   return "redirect:saveUserProfile";*/
 			   }else{
-				   request.setAttribute("message", "failed");
+				   request.setAttribute("message", "success");
 				   return "otpPage";
 			   }
-			   
 			
 		} catch (Exception e) {
 		   e.printStackTrace();
@@ -3967,7 +3979,7 @@ String sJson="";
 				   //boolean success = objUsersDao.saveOtp(sessionBean.getId()+"",mobileNum,otp);
 				   if(StringUtils.isNotBlank(otp)){
 					   try{
-						   String response = SendSMS.sendSMS("OTP for your registration is: "+otp, mobileNum);
+						   String response = SendSMS.sendSMS("OTP for your registration is: "+otp+"\n\n --Team AarnaMatrimony", mobileNum);
 						   
 						   if("OK".equalsIgnoreCase(response)){
 							   objJson.put("message", "success");
