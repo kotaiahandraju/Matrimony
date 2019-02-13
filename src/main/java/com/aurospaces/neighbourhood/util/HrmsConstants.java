8a2a33cdea7b84d041b70a54912c2b2d7dfcc4b8
kotaiahandraju <31728830+kotaiahandraju@users.noreply.github.com>
2019-02-13 12:44:45 +0530

    Delete HrmsConstants.java

--- src/main/java/com/aurospaces/neighbourhood/util/HrmsConstants.java
+++
@@ -1,8 +0,0 @@
-package com.aurospaces.neighbourhood.util;
-
-
-public class HrmsConstants {
-	public static final int ADMIN = 1;
-	public static final int ROLE_MANAGER = 2;
-	public static final int ROLE_TEAM_MEMBER = 5;
-}
